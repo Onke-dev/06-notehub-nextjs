964208b9e4324556bfa38da7466d1fed3e5991bd
Onke-dev <nadyushka.nats@gmail.com>
2026-05-24 23:52:04 +0300

    init update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error -- importing global CSS in app layout
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreDuringBuilds: true,
+  reactCompiler: true,
 };
 
 export default nextConfig;
